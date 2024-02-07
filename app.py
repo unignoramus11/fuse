50030dd778314903e193b5fb95e22d497e2779a9
Mohit Singh <unignoramus@gmail.com>
2024-02-07 22:37:45 +0530

    Hotfix: Update login and register pages and add admin page access
    Co-authored-by: Aviral <avi1o1@users.noreply.github.com>
    Co-authored-by: Mohit Singh <unignoramus11@users.noreply.github.com>

--- app.py
+++ app.py
@@ -11,13 +11,17 @@ def home():
 @app.route("/signin", methods=["POST", "GET"])
 def signin():
     if request.method == "POST":
-        email = request.form["email"]
+        username = request.form["username"]
         password = request.form["password"]
 
         # Fetch user from database
-        user = email
+        # TODO: make it so that it send the name instead of username
+        name = username
 
-        return redirect(url_for("profile", usr=user))
+        if name == "admin" and password == "admin":
+            return redirect(url_for("admin"))
+
+        return redirect(url_for("profile", name=name))
     else:
         return render_template("sign-in.html")
 
@@ -25,12 +29,13 @@ def signin():
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
     if request.method == "POST":
+        username = request.form["username"]
         name = request.form["name"]
         email = request.form["email"]
         password = request.form["password"]
 
         # Create a user
-        user = {"name": name, "email": email, "password": password}
+        user = {"username": username, "name": name, "email": email, "password": password}
 
         # Add user to database
 
@@ -45,9 +50,9 @@ def admin():
     return render_template("admin-dashboard.html")
 
 
-@app.route("/profile/<usr>")
-def profile(usr):
-    return render_template("profile.html", user=usr)
+@app.route("/profile/<name>")
+def profile(name):
+    return render_template("profile.html", name=name)
 
 
 @app.route("/project-editor")
